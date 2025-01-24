734e61985f9ed4b01b7ba0b658fd93812ae5e583
HareeshSimma <hsimma@innominds.com>
2025-01-24 14:36:58 +0530

    added few functionalities

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-them='lemonade'>
       <body className={inter.className}>{children}</body>
     </html>
   )
---
+++ app/users/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+const UsersPage = async () => {
+  // const res = await fetch('https://jsonplaceholder.typicode.com/users', {next: { revalidate: 10 }});
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'});
+
+  const users: User[] = await res.json();
+  return (
+    <div>
+      <h1>Users:</h1>
+      {/* <p>{new Date().toLocaleTimeString()}</p> */}
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody> 
+        {users.map(user => <tr key={user.id}> 
+        <td>{user.id}</td>
+        <td>{user.name}</td>
+        <td>{user.email}</td> 
+        </tr>)}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default UsersPage
